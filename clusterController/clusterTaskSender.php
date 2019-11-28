d33e5f47eeabacee8148627a20f9ce9deab023d3
ferko2019 <55391580+ferko2019@users.noreply.github.com>
2019-11-28 16:08:51 +0100

    sad
    asd

---
+++ clusterController/clusterTaskSender.php
@@ -0,0 +1,13 @@
+<?php
+$file = $_POST["file"];
+
+$clusterNodes = array("ip1", "ip2", "ip3");
+
+$workingclusters = array("false");
+
+for(i = 0; i < count($clusterNodes); i++){
+    array_push($workingclusters,'false');
+}
+
+echo $workingclusters;
+?>
